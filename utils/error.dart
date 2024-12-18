d599e1fc0de8d61c0225caca0664c02e8593e4a3
ARUNESH PURKAIT <aruneshpurkait99@gmail.com>
2024-12-19 01:36:20 +0530

    Add files via upload

--- utils/error.dart
+++ utils/error.dart
@@ -2,57 +2,83 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart';
+import 'package:utils/utils.dart';
 
 import '../data/remote/response/error_response.dart';
 import '../domain/model/exception.dart';
+import '../generated/l10n.dart';
 
-String getErrorMessage(dynamic error) {
-  if (error is String) {
-    return error;
-  }
+@deprecated
+String getErrorMessageDeprecated(Object error) =>
+    S.current.getErrorMessage(error);
 
-  // domain errors
-  if (error is NotCompletedLoginException) {
-    return 'Required updating your profile';
-  }
-  if (error is NotLoggedInException) {
-    return 'Not logged in';
-  }
-  if (error is WrongRoleException) {
-    return 'Only ADMIN or STAFF role is allowed';
-  }
+extension ErrorMessageS on S {
+  String get _unknownError => 'Unknown error'; // TODO: l10n
 
-  // server error
-  if (error is SingleMessageErrorResponse) {
-    return error.message;
-  }
-  if (error is MultipleMessagesErrorResponse) {
-    return error.messages.join('\n');
-  }
+  String getErrorMessage(Object error) {
+    ArgumentError.checkNotNull(error);
 
-  // network error
-  if (error is SocketException) {
-    return 'No internet connection';
-  }
-  if (error is TimeoutException) {
-    return 'Slow internet connection';
-  }
-  if (error is HttpException) {
-    return 'Network error';
-  }
-  if (error is ClientException) {
-    return 'Network error';
-  }
+    if (error is String) {
+      return error;
+    }
 
-  // firebase & platform errors
-  if (error is FirebaseAuthException) {
-    return error.message;
-  }
-  if (error is PlatformException) {
-    return error.message;
-  }
+    // domain errors
+    if (error is NotCompletedLoginException) {
+      return requiredUpdatingYourProfile;
+    }
+    if (error is NotLoggedInException) {
+      return notLoggedIn;
+    }
+    if (error is NotVerifiedEmail) {
+      return yourAccountEmailHasNotBeenVerifyPleaseVerifyTo;
+    }
+    if (error is WrongRoleException) {
+      return onlyUserRoleIsAllowed;
+    }
 
-  return 'Error: $error';
+    // server error
+    if (error is SingleMessageErrorResponse) {
+      return error.message ?? _unknownError;
+    }
+    if (error is MultipleMessagesErrorResponse) {
+      return error.messages.isNullOrEmpty
+          ? _unknownError
+          : error.messages!.join('\n');
+    }
+
+    // network error
+    if (error is SocketException) {
+      return noInternetConnection;
+    }
+    if (error is TimeoutException) {
+      return slowInternetConnection;
+    }
+    if (error is HttpException) {
+      return networkError;
+    }
+    if (error is ClientException) {
+      return networkError;
+    }
+
+    // firebase & platform errors
+    if (error is FirebaseAuthException) {
+      return error.message ?? _unknownError;
+    }
+    if (error is PlatformException) {
+      return error.message ?? _unknownError;
+    }
+
+    return error.toString();
+  }
+}
+
+extension ErrorMessageBuildContext on BuildContext {
+  String getErrorMessage(Object error) => S.of(this).getErrorMessage(error);
+}
+
+extension ErrorMessageState on State {
+  String getErrorMessage(Object error) => context.getErrorMessage(error);
 }
